39550fbbeda9d2aec968aefa878effd34f4f8ebb
tom@neoverve.com <development@neoverve.net>
2015-08-24 17:16:17 -0700

    Refine homepage animations and spacing

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -3,6 +3,7 @@
 */
 
 (function(){
+	// scroll to top of page on refresh
 	$(window).on('beforeunload', function() {
 	    if($('.homepage').length){
 	    	$(window).scrollTop(0);
@@ -18,34 +19,74 @@
 		});
 		$('#topPage').css('display', 'none');
 
-		//$('.home .content').css("min-height", "650px");
-		$('.home .content, .about .content').css({
-				"opacity" : "0",
+		if($('.homepage').length){	
+			// set animation start points
+			$('.home .content img, .content h2, .content p, .content .button,  .about .content svg').css({
+					"opacity" : "0"				
+				});
+			$('.home .content img, .about .content svg').css({
 				"margin-top" : "100px"
 			});
-		$('.hero h1').css({
-			"opacity" : "0"
-		});
-		$('.hero h1').stop().animate({
-			opacity: 1
-		}, 600);
+			$('.hero h1').css({
+				"opacity" : "0"
+			});
+			$('.hero h1').stop().delay(200).animate({
+				opacity: 1
+			}, 600);
+		}
 
+		
 		
 	});
 
 	$(window).scroll(function () {
-		if($('.homepage').length){			
-			if($(window).scrollTop() > 70 ){	
-				$('.home .content').animate({
-					opacity: 1,
-					'margin-top' : "50px"
-				}, 300);
+		var windowWidth = $(this).width();
+	    var windowHeight = $(this).height();
+	    var windowScrollTop = $(this).scrollTop();
+	    var homeTarget = $('.home');
+	    var aboutTarget = $('.about');
+
+		if($('.homepage').length){		
+			var animateContent = function(){
+				$('.home .content h2').animate({
+					opacity: 1
+				},600);
+
+				$('.home .content p').animate({
+					opacity: 1
+				},600);
+				$('.home .content .button').delay(400).animate({
+					opacity: 1
+				},600);
 			}
-			if($(window).scrollTop() > 550 ){	
-				$('.about .content').animate({
+			var animateAbout = function(){
+				$('.about .content h2').animate({
+					opacity: 1
+				},600);
+
+				$('.about .content p').animate({
+					opacity: 1
+				},600);
+				$('.about .content .button').delay(400).animate({
+					opacity: 1
+				},600);
+			}	
+			if(windowScrollTop + windowHeight > homeTarget.position().top){	
+				$('.home .content img').animate({
 					opacity: 1,
-					'margin-top' : "50px"
-				}, 300);
+					'margin-top' : "0px"
+				}, 300, function(){
+					animateContent();
+					
+				});
+			}
+			if(windowScrollTop + windowHeight > aboutTarget.position().top ){	
+				$('.about .content svg').delay(200).animate({
+					opacity: 1,
+					'margin-top' : "0px"
+				}, 300, function(){
+					animateAbout();
+				});
 			}
 		}	
 		if($(window).scrollTop() > 800 ){		    	
