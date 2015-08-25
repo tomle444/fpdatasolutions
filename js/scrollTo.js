386700a9425e7f4a67196c88f073b5876b15408f
tom@neoverve.com <development@neoverve.net>
2015-08-24 17:44:33 -0700

    eliminate animations for mobile devices

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -46,47 +46,49 @@
 	    var homeTarget = $('.home');
 	    var aboutTarget = $('.about');
 
-		if($('.homepage').length){		
-			var animateContent = function(){
-				$('.home .content h2').animate({
-					opacity: 1
-				},600);
+		if($('.homepage').length){ 
+			if(windowWidth >= 768){	 
+				var animateContent = function(){
+					$('.home .content h2').animate({
+						opacity: 1
+					},600);
 
-				$('.home .content p').animate({
-					opacity: 1
-				},600);
-				$('.home .content .button').delay(400).animate({
-					opacity: 1
-				},600);
-			}
-			var animateAbout = function(){
-				$('.about .content h2').animate({
-					opacity: 1
-				},600);
+					$('.home .content p').animate({
+						opacity: 1
+					},600);
+					$('.home .content .button').delay(400).animate({
+						opacity: 1
+					},600);
+				}
+				var animateAbout = function(){
+					$('.about .content h2').animate({
+						opacity: 1
+					},600);
 
-				$('.about .content p').animate({
-					opacity: 1
-				},600);
-				$('.about .content .button').delay(400).animate({
-					opacity: 1
-				},600);
-			}	
-			if(windowScrollTop + windowHeight > homeTarget.position().top){	
-				$('.home .content img').animate({
-					opacity: 1,
-					'margin-top' : "0px"
-				}, 300, function(){
-					animateContent();
-					
-				});
-			}
-			if(windowScrollTop + windowHeight > aboutTarget.position().top ){	
-				$('.about .content svg').delay(200).animate({
-					opacity: 1,
-					'margin-top' : "0px"
-				}, 300, function(){
-					animateAbout();
-				});
+					$('.about .content p').animate({
+						opacity: 1
+					},600);
+					$('.about .content .button').delay(400).animate({
+						opacity: 1
+					},600);
+				}	
+				if(windowScrollTop + windowHeight > homeTarget.position().top){	
+					$('.home .content img').animate({
+						opacity: 1,
+						'margin-top' : "0px"
+					}, 300, function(){
+						animateContent();
+						
+					});
+				}
+				if(windowScrollTop + windowHeight > aboutTarget.position().top ){	
+					$('.about .content svg').delay(200).animate({
+						opacity: 1,
+						'margin-top' : "0px"
+					}, 300, function(){
+						animateAbout();
+					});
+				}
 			}
 		}	
 		if($(window).scrollTop() > 800 ){		    	
